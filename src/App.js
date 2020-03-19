c5a838625ff1b5c4b2c59e98cd2294bd8f8babec
Timothy Shores <timothymshores@gmail.com>
2020-03-19 12:59:13 -0500

    First axios-hooks get request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";``
-import "./App.css";
+import React from "react";
+import useAxios from "axios-hooks";
 
 const App = () => {
+	const [{ data, loading, error }, refetch] = useAxios(
+		"https://jsonplaceholder.typicode.com/users/"
+	);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error!</p>;
+
 	return (
 		<div className="App">
 			<h1>Case Management</h1>
+			<button onClick={refetch}>refetch</button>
+			<pre>{JSON.stringify(data, null, 2)}</pre>
 		</div>
 	);
 };
